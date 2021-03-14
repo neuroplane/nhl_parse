b5ddc1cf8475eaa30a6cae57765808c1e63cfb22
neuroplane <aleksey@boyarkin.com>
2021-03-14 15:40:22 +0300

    Update

--- append.py
+++ append.py
@@ -12,14 +12,15 @@ new_task = 'new task'
 new_comment = 'new comment'
 
 new_recurrent = {'task_first_date': new_date, 'task_time': new_time, 'task_name': new_task, 'comment': new_comment}
-new_data
+new_data = None
 
 with open('calendar.json') as cal:
     data = json.load(cal)
     print(data)
     data['recurrent'].append(new_recurrent)
+    data['birthdays'] = []
     new_data = data['recurrent']
-    print(new_data)
+    #print(new_data)
     #json.dump(new_data, cal, ensure_ascii=False, indent=2)
-
+print(data)
 #print(new_date + new_time + new_task + new_comment)
