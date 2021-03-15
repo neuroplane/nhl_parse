fbf13a097f7b4908efd288de52b2ecf3c4c12073
neuroplane <aleksey@boyarkin.com>
2021-03-15 13:16:48 +0300

    Commit

--- append.py
+++ append.py
@@ -1,26 +1,32 @@
+# Python program to update 
+# JSON 
+
 import json
-from datetime import datetime
 
-#new_date = input('Введите дату:\t')
-#new_time = input('Введите время:\t')
-#new_task = input('Введите имя задания:\t')
-#new_comment = input('Введите комментарий:\t')
+# function to add to JSON 
+def write_json(data, filename='calendar.json'):
+    with open(filename, 'w') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
 
-new_date = 'New date'
-new_time = 'new time'
-new_task = 'new task'
-new_comment = 'new comment'
+new_date = input('Date:\t')
+new_time = input('Time:\t')
+new_task = input('Task:\t')
+new_comment = input('Comment:\t')
 
-new_recurrent = {'task_first_date': new_date, 'task_time': new_time, 'task_name': new_task, 'comment': new_comment}
-new_data = None
+with open('calendar.json') as json_file:
+    data = json.load(json_file)
 
-with open('calendar.json') as cal:
-    data = json.load(cal)
-    print(data)
-    data['recurrent'].append(new_recurrent)
-    data['birthdays'] = []
-    new_data = data['recurrent']
-    #print(new_data)
-    #json.dump(new_data, cal, ensure_ascii=False, indent=2)
-print(data)
-#print(new_date + new_time + new_task + new_comment)
+    temp = data['recurrent']
+
+    # python object to be appended
+    y = {"task_first_date": new_date,
+         "task_time": new_time,
+         "task_name": new_task,
+         "comment": new_comment
+         }
+
+    # appending data to emp_details
+    temp.append(y)
+
+write_json(data) 
+
