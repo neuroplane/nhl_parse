5dd4af3a08f899b670f431ba908f4dc86d457132
neuroplane <aleksey@boyarkin.com>
2021-03-15 14:56:01 +0300

    Commit

--- append.py
+++ append.py
@@ -1,9 +1,6 @@
-# Python program to update 
-# JSON 
 
 import json
 
-# function to add to JSON 
 def write_json(data, filename='calendar.json'):
     with open(filename, 'w') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
@@ -15,18 +12,13 @@ new_comment = input('Comment:\t')
 
 with open('calendar.json') as json_file:
     data = json.load(json_file)
-
     temp = data['recurrent']
-
-    # python object to be appended
     y = {"task_first_date": new_date,
          "task_time": new_time,
          "task_name": new_task,
          "comment": new_comment
          }
-
-    # appending data to emp_details
     temp.append(y)
 
-write_json(data) 
+write_json(data)
 
