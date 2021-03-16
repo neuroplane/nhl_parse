c7834a2930deca57e8440c8ef01c1a14e4798f9f
neuroplane <aleksey@boyarkin.com>
2021-03-16 17:29:48 +0300

    Commit

--- scores_for_nhl_highlights.py
+++ scores_for_nhl_highlights.py
@@ -14,21 +14,25 @@ yesterday_rus = (datetime.today() - timedelta(days=1)).strftime('%d.%m.%Y')
 last_games = requests.get(
     "https://statsapi.web.nhl.com/api/v1/schedule?startDate=" + yesterday + "&endDate=" + yesterday + "&hydrate=team,linescore,broadcasts(all),tickets,game(content(media(epg)),seriesSummary),radioBroadcasts,metadata,seriesSummary(series)&site=ru_nhl&teamId=&gameType=&timecode=").json()
 last_games_parsed = jmespath.search(
-    "dates[].games[].teams[].{away: {team: away.team.teamName, loc: away.team.locationName, score: away.score},home:{team: home.team.teamName, loc: home.team.locationName, score: home.score}}",
+    "dates[].games[].{otstatus: linescore. currentPeriod, away: {team: teams.away.team.teamName, loc: teams.away.team.locationName, score: teams.away.score},home:{team: teams.home.team.teamName, loc: teams.home.team.locationName, score: teams.home.score}}",
     last_games)
 
 with Image.open("pics/800.png") as im:
     SCW = 800
     SCH = 800
-    LINE_HEIGHT = 40
-    START_Y_SCORES = 150
-    GREY = (220, 220, 220)
+    LINE_HEIGHT = 35
+    START_Y_SCORES = 200
+    GREY = (220, 220, 220, 128)
     SHADOW = (50, 50, 50)
     boston = ImageFont.truetype('fonts/nhlboston.ttf', 24)
     robotocond = ImageFont.truetype('fonts/robotocond.ttf', 28)
     mach = ImageFont.truetype('fonts/mach.otf', 33)
+    machsmall = ImageFont.truetype('fonts/mach.otf', 20)
+    machbig = ImageFont.truetype('fonts/mach.otf', 60)
     def_font = mach
     draw = ImageDraw.Draw(im)
+    draw.rectangle([(0, 40), (800, 120)], fill=(0, 0, 0, 228), outline=None)
+    draw.text((400, 80), 'РЕЗУЛЬТАТЫ ЗА ' + str(yesterday_rus), font=machbig, fill='white', anchor='mm')
     for item in last_games_parsed:
         line_away = str.upper(item['away']['loc']) + "  " + str.upper(item['away']['team'])
         line_score = str(item['away']['score']) + " : " + str(item['home']['score'])
@@ -42,6 +46,8 @@ with Image.open("pics/800.png") as im:
         ################################### TEXT
         draw.text((SCW / 2 - 70, START_Y_SCORES), line_away, font=def_font, fill=GREY, anchor="rm")
         draw.text((SCW / 2, START_Y_SCORES), line_score, font=def_font, fill=GREY, anchor="mm")
+        if item['otstatus'] == 4:
+            draw.text((SCW / 2 + 30, START_Y_SCORES - 1 + 7), 'OT', font=machsmall, fill=GREY, anchor="lb")
         draw.text((SCW / 2 + 70, START_Y_SCORES), line_home, font=def_font, fill=GREY, anchor="lm")
 
         START_Y_SCORES = START_Y_SCORES + LINE_HEIGHT
