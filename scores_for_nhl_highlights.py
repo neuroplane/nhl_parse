e21e5be48aaf37ea1eb5c7a28aab47ecd369dca5
neuroplane <aleksey@boyarkin.com>
2021-03-16 16:42:31 +0300

    Commit

---
+++ scores_for_nhl_highlights.py
@@ -0,0 +1,49 @@
+import os
+import sys
+
+import jmespath
+import requests
+import PIL
+from PIL import Image, ImageDraw, ImageFont, ImageOps
+from datetime import datetime, timezone, timedelta
+##########################################################
+now = datetime.today().strftime('%H:%M:%S')
+yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+yesterday_rus = (datetime.today() - timedelta(days=1)).strftime('%d.%m.%Y')
+##########################################################
+last_games = requests.get(
+    "https://statsapi.web.nhl.com/api/v1/schedule?startDate=" + yesterday + "&endDate=" + yesterday + "&hydrate=team,linescore,broadcasts(all),tickets,game(content(media(epg)),seriesSummary),radioBroadcasts,metadata,seriesSummary(series)&site=ru_nhl&teamId=&gameType=&timecode=").json()
+last_games_parsed = jmespath.search(
+    "dates[].games[].teams[].{away: {team: away.team.teamName, loc: away.team.locationName, score: away.score},home:{team: home.team.teamName, loc: home.team.locationName, score: home.score}}",
+    last_games)
+
+with Image.open("pics/800.png") as im:
+    SCW = 800
+    SCH = 800
+    LINE_HEIGHT = 40
+    START_Y_SCORES = 150
+    GREY = (220, 220, 220)
+    SHADOW = (50, 50, 50)
+    boston = ImageFont.truetype('fonts/nhlboston.ttf', 24)
+    robotocond = ImageFont.truetype('fonts/robotocond.ttf', 28)
+    mach = ImageFont.truetype('fonts/mach.otf', 33)
+    def_font = mach
+    draw = ImageDraw.Draw(im)
+    for item in last_games_parsed:
+        line_away = str.upper(item['away']['loc']) + "  " + str.upper(item['away']['team'])
+        line_score = str(item['away']['score']) + " : " + str(item['home']['score'])
+        line_home = str.upper(item['home']['loc']) + "  " + str.upper(item['home']['team'])
+        # print(w)
+        # draw.rectangle((START_X,START_Y, 11, 11), fill = 0)
+        ################################### SHADOW
+        draw.text((SCW / 2 - 70 - 1 , START_Y_SCORES-1), line_away, font=def_font, fill=SHADOW, anchor="rm")
+        draw.text((SCW / 2 - 1, START_Y_SCORES-1), line_score, font=def_font, fill=SHADOW, anchor="mm")
+        draw.text((SCW / 2 + 70 - 1, START_Y_SCORES-1), line_home, font=def_font, fill=SHADOW, anchor="lm")
+        ################################### TEXT
+        draw.text((SCW / 2 - 70, START_Y_SCORES), line_away, font=def_font, fill=GREY, anchor="rm")
+        draw.text((SCW / 2, START_Y_SCORES), line_score, font=def_font, fill=GREY, anchor="mm")
+        draw.text((SCW / 2 + 70, START_Y_SCORES), line_home, font=def_font, fill=GREY, anchor="lm")
+
+        START_Y_SCORES = START_Y_SCORES + LINE_HEIGHT
+    # write to stdout
+    im.show()
