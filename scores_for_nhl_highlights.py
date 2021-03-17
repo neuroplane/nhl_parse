4fb90fb0585febde01fc73dd3544ab4c8f46d25c
neuroplane <aleksey@boyarkin.com>
2021-03-17 13:20:36 +0300

    Commit

--- scores_for_nhl_highlights.py
+++ scores_for_nhl_highlights.py
@@ -20,23 +20,24 @@ last_games_parsed = jmespath.search(
 with Image.open("pics/800.png") as im:
     SCW = 800
     SCH = 800
-    LINE_HEIGHT = 35
+    LINE_HEIGHT = 45
     START_Y_SCORES = 200
     GREY = (220, 220, 220, 128)
     SHADOW = (50, 50, 50)
     boston = ImageFont.truetype('fonts/nhlboston.ttf', 24)
     robotocond = ImageFont.truetype('fonts/robotocond.ttf', 28)
-    mach = ImageFont.truetype('fonts/mach.otf', 33)
-    machsmall = ImageFont.truetype('fonts/mach.otf', 20)
+    mach = ImageFont.truetype('fonts/mach.otf', 40)
+    machsmall = ImageFont.truetype('fonts/mach.otf', 25)
     machbig = ImageFont.truetype('fonts/mach.otf', 60)
+    unreal = ImageFont.truetype('fonts/unreal.ttf', 60)
     def_font = mach
     draw = ImageDraw.Draw(im)
     draw.rectangle([(0, 40), (800, 120)], fill=(0, 0, 0, 228), outline=None)
     draw.text((400, 80), 'РЕЗУЛЬТАТЫ ЗА ' + str(yesterday_rus), font=machbig, fill='white', anchor='mm')
     for item in last_games_parsed:
-        line_away = str.upper(item['away']['loc']) + "  " + str.upper(item['away']['team'])
+        line_away = str.strip(item['away']['loc'].upper()) #+ "  " + str.upper(item['away']['team'])
         line_score = str(item['away']['score']) + " : " + str(item['home']['score'])
-        line_home = str.upper(item['home']['loc']) + "  " + str.upper(item['home']['team'])
+        line_home = str.upper(item['home']['loc']) #+ "  " + str.upper(item['home']['team'])
         # print(w)
         # draw.rectangle((START_X,START_Y, 11, 11), fill = 0)
         ################################### SHADOW
@@ -47,9 +48,12 @@ with Image.open("pics/800.png") as im:
         draw.text((SCW / 2 - 70, START_Y_SCORES), line_away, font=def_font, fill=GREY, anchor="rm")
         draw.text((SCW / 2, START_Y_SCORES), line_score, font=def_font, fill=GREY, anchor="mm")
         if item['otstatus'] == 4:
-            draw.text((SCW / 2 + 29, START_Y_SCORES - 1 + 5), 'OT', font=machsmall, fill=GREY, anchor="lb")
+            draw.text((SCW / 2 + 33, START_Y_SCORES - 1 + 5), 'OT', font=machsmall, fill=GREY, anchor="lb")
+        elif item['otstatus'] == 5:
+            draw.text((SCW / 2 + 33, START_Y_SCORES - 1 + 5), 'Б', font=machsmall, fill=GREY, anchor="lb")
         draw.text((SCW / 2 + 70, START_Y_SCORES), line_home, font=def_font, fill=GREY, anchor="lm")
 
         START_Y_SCORES = START_Y_SCORES + LINE_HEIGHT
     # write to stdout
+    print(last_games_parsed)
     im.show()
