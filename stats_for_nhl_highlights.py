d15e26439ab1094c5c3a04333c99c93067f0e50a
neuroplane <aleksey@boyarkin.com>
2021-03-19 17:30:35 +0300

    Commit

--- stats_for_nhl_highlights.py
+++ stats_for_nhl_highlights.py
@@ -9,6 +9,8 @@ from PIL import Image, ImageDraw, ImageFont, ImageOps
 from datetime import datetime, timezone, timedelta
 ##########################################################
 now = datetime.today().strftime('%H:%M:%S')
+now_file = datetime.today().strftime('%H%M%S')
+now_date = datetime.today().strftime('%d.%m.%Y')
 yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
 yesterday_rus = (datetime.today() - timedelta(days=1)).strftime('%d.%m.%Y')
 #SCORES#########################################################
@@ -21,7 +23,7 @@ last_games_parsed = jmespath.search(
 field_players = requests.get(
     "https://api.nhle.com/stats/rest/ru/skater/summary?isAggregate=false&isGame=false&sort=%5B%7B%22property%22:%22points%22,%22direction%22:%22DESC%22%7D%5D&start=0&limit=50&factCayenneExp=gamesPlayed%3E=1&cayenneExp=gameTypeId=2%20and%20seasonId%3C=20202021%20and%20seasonId%3E=20202021").json()
 field_players_parsed = jmespath.search(
-    "data[].{name: skaterFullName, team: teamAbbrevs, position: positionCode, goals: goals, assists: assists, points: points, plusminus: plusMinus}",
+    "data[].{name: skaterFullName, ppg: ppGoals, gp: gamesPlayed, toi: timeOnIcePerGame, team: teamAbbrevs, position: positionCode, goals: goals, assists: assists, points: points, plusminus: plusMinus}",
     field_players)
 #STATS#########################################################
 field_players_rus = requests.get(
@@ -35,22 +37,26 @@ print(field_players_parsed)
 #SCORES########################################################
 #SCORES########################################################
 #with Image.open("pics/stats.png") as im:
-with Image.open("pics/stanley.png") as im:
-    SCW = 1668
-    SCH = 2388
-    LINE_HEIGHT = 63
+with Image.open("pics/highlights.jpg") as im:
+    SCW, SCH = im.size
+    print(im.size)
+    #SCW = 1668
+    #SCH = 2388
+    LINE_HEIGHT = 85
     LINE_H = 60
     START_Y_SCORES = 100
-    GREY = (220, 220, 220, 128)
+    GREY = (230, 230, 230, 128)
     SHADOW = (50, 50, 50)
     boston = ImageFont.truetype('fonts/nhlboston.ttf', 24)
     robotocond = ImageFont.truetype('fonts/robotocond.ttf', 48)
     mach = ImageFont.truetype('fonts/mach.otf', 40)
     machsmall = ImageFont.truetype('fonts/mach.otf', 25)
-    machbig = ImageFont.truetype('fonts/mach.otf', 120)
+    machbig = ImageFont.truetype('fonts/mach.otf', 140)
     unreal = ImageFont.truetype('fonts/unreal.ttf', 50)
-    ubuntu = ImageFont.truetype('fonts/Ubuntu-M.ttf', 55)
+    ubuntu = ImageFont.truetype('fonts/Ubuntu-M.ttf', 65)
     ubuntuс = ImageFont.truetype('fonts/ubuntuc.ttf', 65)
+    kroftsman = ImageFont.truetype('fonts/kroftsman.ttf', 150)
+    kroftsmansm = ImageFont.truetype('fonts/kroftsman.ttf', 150)
     def_font = ubuntu
     draw = ImageDraw.Draw(im)
     #draw.rectangle([(0, 40), (SCW, 120)], fill=(0, 0, 0, 228), outline=None)
@@ -58,17 +64,17 @@ with Image.open("pics/stanley.png") as im:
     ###### FIELD PLAYERS DRAW
     START_Y_FIELDPLAYERS = START_Y_SCORES + LINE_HEIGHT
     draw.line((50,  START_Y_FIELDPLAYERS+LINE_HEIGHT, SCW - 50, START_Y_FIELDPLAYERS+LINE_HEIGHT), fill=GREY, width=1)
-    draw.text((SCW / 2, START_Y_FIELDPLAYERS-30), 'БОМБАРДИРЫ ЛИГИ', font=machbig, fill=GREY, anchor="mm")
+    draw.text((SCW / 2, START_Y_FIELDPLAYERS-30), 'БОМБАРДИРЬ', font=kroftsman, fill=GREY, anchor="mm")
     START_Y_FIELDPLAYERS = START_Y_FIELDPLAYERS + LINE_HEIGHT
     draw.line((50, START_Y_FIELDPLAYERS + LINE_HEIGHT*2, SCW - 50, START_Y_FIELDPLAYERS + LINE_HEIGHT*2), fill=GREY,
               width=1)
-    pos_num = 100
-    pos_position = pos_num + 80
-    pos_name = pos_position + 100
-    pos_team = pos_name + 700
-    pos_goals = pos_team + 350  # 180
+    pos_num = round(SCW*0.07)
+    pos_position = round(SCW * 0.12)
+    pos_name = round(SCW * 0.19)
+    pos_team = round(SCW * 0.65)
+    pos_goals = round(SCW * 0.84)  # 180
     #pos_assists = pos_goals + 190  # 210
-    pos_points = pos_goals + 200  # 240
+    pos_points = round(SCW * 0.96)  # 240
     pos_plusminus = pos_points + 250  # 270
     START_Y_FIELDPLAYERS = START_Y_FIELDPLAYERS + LINE_HEIGHT
     draw.text((pos_num, START_Y_FIELDPLAYERS), '#', font=def_font, fill=GREY, anchor="rm")
@@ -79,7 +85,7 @@ with Image.open("pics/stanley.png") as im:
     #draw.text((pos_assists, START_Y_FIELDPLAYERS), "П", font=def_font, fill=GREY, anchor="lm")
     draw.text((pos_points, START_Y_FIELDPLAYERS), "О", font=def_font, fill=GREY, anchor="rm")
     #draw.text((pos_plusminus, START_Y_FIELDPLAYERS), "+/-", font=def_font, fill=GREY, anchor="rm")
-    START_Y_FIELDPLAYERS = START_Y_FIELDPLAYERS + 120
+    START_Y_FIELDPLAYERS = START_Y_FIELDPLAYERS + LINE_HEIGHT*2
     for index, item in zip(range(10), field_players_parsed):
         draw.text((pos_num, START_Y_FIELDPLAYERS), str(index + 1), font=def_font, fill=GREY, anchor="rm")
         draw.text((pos_name, START_Y_FIELDPLAYERS), item['name'], font=def_font, fill=GREY, anchor="lm")
@@ -90,10 +96,12 @@ with Image.open("pics/stanley.png") as im:
         draw.text((pos_points, START_Y_FIELDPLAYERS), str(item['points']), font=def_font, fill=GREY, anchor="rm")
         #draw.text((pos_plusminus, START_Y_FIELDPLAYERS), str(item['plusminus']), font=def_font, fill=GREY, anchor="rm")
         START_Y_FIELDPLAYERS = START_Y_FIELDPLAYERS + LINE_HEIGHT
+        toi = str(round(item['toi']*item['gp']/item['goals']/60, 2))
+        print(item['name'] + " " + toi)
 
-    START_Y_FIELDPLAYERS_RUS = START_Y_FIELDPLAYERS + LINE_HEIGHT*3
+    START_Y_FIELDPLAYERS_RUS = START_Y_FIELDPLAYERS + LINE_HEIGHT*2
     draw.line((50, LINE_HEIGHT + START_Y_FIELDPLAYERS_RUS, SCW - 50, LINE_HEIGHT + START_Y_FIELDPLAYERS_RUS), fill=GREY, width=1)
-    draw.text((SCW / 2, START_Y_FIELDPLAYERS_RUS-30), 'БОМБАРДИРЫ ЛИГИ ИЗ РОССИИ', font=machbig, fill=GREY, anchor="mm")
+    draw.text((SCW / 2, START_Y_FIELDPLAYERS_RUS-30), 'БОМБАРДИРЬ ИЗ РОССИИ', font=kroftsman, fill=GREY, anchor="mm")
     START_Y_FIELDPLAYERS_RUS = START_Y_FIELDPLAYERS_RUS + LINE_HEIGHT * 2
     draw.line((50, START_Y_FIELDPLAYERS_RUS + LINE_HEIGHT, SCW - 50, START_Y_FIELDPLAYERS_RUS + LINE_HEIGHT), fill=GREY,
               width=1)
@@ -103,7 +111,7 @@ with Image.open("pics/stanley.png") as im:
     draw.text((pos_position, START_Y_FIELDPLAYERS_RUS), "П", font=def_font, fill=GREY, anchor="lm")
     draw.text((pos_goals, START_Y_FIELDPLAYERS_RUS), "Г", font=def_font, fill=GREY, anchor="rm")
     #draw.text((pos_assists, START_Y_FIELDPLAYERS_RUS), "Пас", font=def_font, fill=GREY, anchor="lm")
-    draw.text((pos_points, START_Y_FIELDPLAYERS_RUS), "О", font=def_font, fill=GREY, anchor="lm")
+    draw.text((pos_points, START_Y_FIELDPLAYERS_RUS), "О", font=def_font, fill=GREY, anchor="rm")
     #draw.text((pos_plusminus, START_Y_FIELDPLAYERS_RUS), "+/-", font=def_font, fill=GREY, anchor="rm")
     START_Y_FIELDPLAYERS_RUS = START_Y_FIELDPLAYERS_RUS + LINE_HEIGHT * 2
     for index, item in zip(range(10), field_players_rus_parsed):
@@ -113,10 +121,13 @@ with Image.open("pics/stanley.png") as im:
         draw.text((pos_position, START_Y_FIELDPLAYERS_RUS), item['position'], font=def_font, fill=GREY, anchor="lm")
         draw.text((pos_goals, START_Y_FIELDPLAYERS_RUS), str(item['goals']), font=def_font, fill=GREY, anchor="rm")
         #draw.text((pos_assists, START_Y_FIELDPLAYERS_RUS), str(item['assists']), font=def_font, fill=GREY, anchor="lm")
-        draw.text((pos_points, START_Y_FIELDPLAYERS_RUS), str(item['points']), font=def_font, fill=GREY, anchor="lm")
+        draw.text((pos_points, START_Y_FIELDPLAYERS_RUS), str(item['points']), font=def_font, fill=GREY, anchor="rm")
         #draw.text((pos_plusminus, START_Y_FIELDPLAYERS_RUS), str(item['plusminus']), font=def_font, fill=GREY, anchor="rm")
         START_Y_FIELDPLAYERS_RUS = START_Y_FIELDPLAYERS_RUS + LINE_HEIGHT
-
+    draw.text((70, SCH-60), str(now_date), font=kroftsmansm, fill=GREY, anchor="lb")
     # write to stdout
-    print(json.dumps(field_players_rus_parsed, ensure_ascii=False, indent=2))
-    im.show()
+    #print(json.dumps(field_players_rus_parsed, ensure_ascii=False, indent=2))
+    size = (167*4, 239*4)
+    out = im.resize(size)
+    out.show()
+    out.save('pics/out' + now_file + '.png')
