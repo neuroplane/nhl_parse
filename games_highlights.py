8496451b3d25865fdb7a718e67ec886c4fe9a1ba
neuroplane <aleksey@boyarkin.com>
2021-03-30 16:48:22 +0300

    Commit

--- games_highlights.py
+++ games_highlights.py
@@ -2,7 +2,7 @@ import jmespath
 import requests
 
 games_data = requests.get(
-    "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2021-03-29&endDate=2021-03-29&hydrate=team,linescore,broadcasts(all),game(content(media(epg)),seriesSummary),radioBroadcasts,metadata").json()
+    "https://statsapi.web.nhl.com/api/v1/schedule?startDate=2021-03-29&endDate=2021-03-30&hydrate=team,linescore,broadcasts(all),game(content(media(epg)),seriesSummary),radioBroadcasts,metadata").json()
 
 games_data_parsed = jmespath.search(
     "dates[].games[].{title: content.media.epg[?title=='Extended Highlights']|[0].items|[0].title, hl_id: content.media.epg[?title=='Extended Highlights']|[0].items|[0].id, date: content.media.epg[?title=='Extended Highlights']|[0].items|[0].date, status: status. detailedState}",
@@ -11,5 +11,8 @@ games_data_parsed = jmespath.search(
 print(games_data_parsed)
 n = 1
 for index, item in zip(range(10), games_data_parsed):
-    print(str(n) + ". " + item['title'] + ", https://www.nhl.com/video/c-" + item['hl_id'] + ", " + item['date'] + ", " + item['status'])
-    n = n + 1
+    if (item['status'] == "Final"):
+        print(str(n) + ". " + item['title'] + ", https://www.nhl.com/video/c-" + item['hl_id'] + ", " + item['date'] + ", " + item['status'])
+        n = n + 1
+    else:
+        print(str(n) + ". " + item['status'])
